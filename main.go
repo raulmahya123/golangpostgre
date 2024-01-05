9d31742c946853e64485c2413a7c8a5ac484c3c4
raulmahya <raulmahya11@gmail.com>
2024-01-05 18:59:56 +0700

    push

---
+++ main.go
@@ -0,0 +1,59 @@
+// main.go
+package main
+
+import (
+	"log"
+	"os"
+
+	"golangsidang/models"
+	"golangsidang/repository"
+	"golangsidang/routes"
+	"golangsidang/storage"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup database connection
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User:     os.Getenv("DB_USER"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("error connecting to the database")
+	}
+
+	// Migrate database models
+	err = models.MigrateBooks(db)
+	if err != nil {
+		log.Fatal("error migrating books")
+	}
+
+	// Create a new Fiber app
+
+	// Create a new Fiber app
+	app := fiber.New()
+	repo2 := repository.New(db)
+	routes.UserRoutes(app, repo2)
+	// Create a repository instance using NewRepository
+	repo := repository.NewRepository(db)
+	// Create another repository instance using New
+	// Setup routes with the first repository instance
+	routes.SetupRoutes(app, repo)
+	// Setup routes with the second repository instance
+	// Start the Fiber app
+	log.Fatal(app.Listen(":3000")) // Change the port if needed
+}
---
+++ repository/user.go
@@ -0,0 +1,283 @@
+package repository
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"golangsidang/models"
+	"golangsidang/utils"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Repositorry struct {
+	DB *gorm.DB
+}
+
+func New(db *gorm.DB) *Repositorry {
+	return &Repositorry{DB: db}
+}
+
+func init() {
+	// Load variables from .env file
+	err := godotenv.Load(filepath.Join(".", ".env"))
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+}
+
+func (r *Repositorry) CreateUser(c *fiber.Ctx) error {
+	userRequest := new(models.User)
+	if err := c.BodyParser(userRequest); err != nil {
+		c.Status(fiber.StatusUnprocessableEntity).JSON(
+			&fiber.Map{
+				"status":  "error",
+				"message": "Invalid JSON",
+			})
+		return err
+	}
+
+	// Hash the user's password before saving it to the database
+	hashedPassword, err := utils.HashPassword(userRequest.Password)
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error hashing password"})
+		return err
+	}
+
+	var users models.User
+	r.DB.Where("username = ?", userRequest.Username).First(&users)
+	if users.Username != "" {
+		c.Status(fiber.StatusBadRequest).JSON(
+			&fiber.Map{
+				"status":  "error",
+				"message": "already exist",
+			})
+		return nil
+	}
+
+	r.DB.Where("email = ?", userRequest.Email).First(&users)
+	if users.Email != "" {
+		c.Status(fiber.StatusBadRequest).JSON(
+			&fiber.Map{
+				"status":  "error",
+				"message": "already exist",
+			})
+		return nil
+	}
+
+	//harusnya menggunakna @gmail.com
+	if !endsWithGmail(userRequest.Email) {
+		userRequest.Email += "@gmail.com"
+	}
+	user := models.User{
+		Username:  userRequest.Username,
+		Password:  hashedPassword,
+		Email:     userRequest.Email,
+		Image:     userRequest.Image,
+		Create_at: time.Now().Format(time.RFC3339),
+		Create_by: userRequest.Username,
+		Role:      "user",
+		Delete_at: false,
+	}
+
+	err = r.DB.Create(&user).Error
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error creating user"})
+		return err
+	}
+
+	c.Status(fiber.StatusCreated).JSON(
+		&fiber.Map{
+			"status": "success",
+			"data":   user,
+		})
+	return nil
+}
+func (r *Repositorry) Login(c *fiber.Ctx) error {
+	loginRequest := new(struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	})
+
+	if err := c.BodyParser(loginRequest); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(
+			&fiber.Map{"message": "Invalid request payload"})
+	}
+
+	// Retrieve the user from the database
+	var user models.User
+	err := r.DB.Where("username = ?", loginRequest.Username).First(&user).Error
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(
+			&fiber.Map{"message": "Invalid username or password"})
+	}
+
+	// Check the password
+	if !utils.CheckPasswordHash(loginRequest.Password, user.Password) {
+		return c.Status(fiber.StatusUnauthorized).JSON(
+			&fiber.Map{"message": "Invalid username or password"})
+	}
+
+	// Generate JWT token
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["password"] = user.Password
+	claims["username"] = user.Username
+	claims["exp"] = jwt.TimeFunc().Add(time.Hour * 24).Unix() // Set token expiration time
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "JWT secret not set"})
+	}
+
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error generating JWT token"})
+	}
+	// Return the generated JWT token in the response
+	return c.JSON(&fiber.Map{
+		"token": tokenString,
+	})
+}
+func endsWithGmail(email string) bool {
+	return len(email) >= len("@gmail.com") && email[len(email)-len("@gmail.com"):] == "@gmail.com"
+}
+
+func (r *Repositorry) GetAllUser(c *fiber.Ctx) error {
+	var users []models.User
+	r.DB.Find(&users)
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   users,
+	})
+}
+
+func (r *Repositorry) GetUserByUsername(c *fiber.Ctx) error {
+	username := c.Params("username")
+
+	var user models.User
+	r.DB.Where("username = ?", username).First(&user)
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
+}
+
+func (r *Repositorry) GetUserById(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var user models.User
+	r.DB.Where("id = ?", id).First(&user)
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
+}
+
+func (r *Repositorry) UpdateUserByid(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	userRequest := new(models.User)
+	if err := c.BodyParser(userRequest); err != nil {
+		c.Status(fiber.StatusUnprocessableEntity).JSON(
+			&fiber.Map{
+				"status":  "error",
+				"message": "Invalid JSON",
+			})
+		return err
+	}
+
+	var user models.User
+	r.DB.Where("id = ?", id).First(&user)
+
+	user.Username = userRequest.Username
+	user.Email = userRequest.Email
+	user.Image = userRequest.Image
+	user.Update_by = userRequest.Username
+
+	err := r.DB.Save(&user).Error
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error updating user by id"})
+		return err
+	}
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
+}
+
+func (r *Repositorry) ResetPaswword(c *fiber.Ctx) error {
+	username := c.Params("username")
+
+	userRequest := new(models.User)
+	if err := c.BodyParser(userRequest); err != nil {
+		c.Status(fiber.StatusUnprocessableEntity).JSON(
+			&fiber.Map{
+				"status":  "error",
+				"message": "Invalid JSON",
+			})
+		return err
+	}
+
+	var user models.User
+	r.DB.Where("username = ?", username).First(&user)
+
+	hashedPassword, err := utils.HashPassword(userRequest.Password)
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error hashing password"})
+		return err
+	}
+
+	user.Password = hashedPassword
+	user.Update_by = userRequest.Username
+
+	err = r.DB.Save(&user).Error
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error updating user"})
+		return err
+	}
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
+}
+
+func (r *Repositorry) DeleteUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var user models.User
+	r.DB.Where("id = ?", id).First(&user)
+
+	user.Delete_at = true
+	user.Delete_by = user.Username
+
+	err := r.DB.Save(&user).Error
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Error updating user"})
+		return err
+	}
+
+	return c.JSON(&fiber.Map{
+		"status": "success",
+		"data":   user,
+	})
+}
